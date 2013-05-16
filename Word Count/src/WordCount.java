40872d31f648fa05317b8432f23121226df3eb09
h4ml3t <meniluca@gmail.com>
2013-05-16 19:19:43 +0200

    update wordcount

--- Word Count/src/WordCount.java
+++ Word Count/src/WordCount.java
@@ -1,29 +1,28 @@
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileInputFormat;
-import org.apache.hadoop.mapred.FileOutputFormat;
-import org.apache.hadoop.mapred.JobClient;
-import org.apache.hadoop.mapred.JobConf;
-
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class WordCount {
 
 	public static void main(String[] args) throws Exception {
-		
-		JobConf conf = new JobConf(WordCount.class);
-		conf.setJobName("Word count");
 
-		conf.setMapperClass(WordCountMapper.class);
-		conf.setReducerClass(WordCountReducer.class);
-		
-		FileInputFormat.addInputPath(conf, new Path("words.txt"));
-		FileOutputFormat.setOutputPath(conf, new Path("output"));
-		
-		conf.setOutputKeyClass(Text.class);
-		conf.setOutputValueClass(IntWritable.class);
+		Job job = new Job(new Configuration(), "WordCount");
 
-		JobClient.runJob(conf);
+		job.setJarByClass(WordCount.class);
+		
+		job.setMapperClass(WordCountMapper.class);
+		job.setReducerClass(WordCountReducer.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		job.waitForCompletion(true);
 	}
-
 }
--- Word Count/src/WordCountReducer.java
+++ Word Count/src/WordCountReducer.java
@@ -1,26 +1,21 @@
 import java.io.IOException;
-import java.util.Iterator;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.MapReduceBase;
-import org.apache.hadoop.mapred.OutputCollector;
-import org.apache.hadoop.mapred.Reducer;
-import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapreduce.Reducer;
 
+public class WordCountReducer extends
+		Reducer<Text, IntWritable, Text, IntWritable> {
 
-public class WordCountReducer
-	extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+	public void reduce(Text key, Iterable<IntWritable> values,
+			Context context) throws IOException, InterruptedException {
 
-	public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter)
-			throws IOException {
-		
 		int sum = 0;
-		
-		while ( values.hasNext() ) {
-			sum += values.next().get();
-		}
-		output.collect(key, new IntWritable(sum));
-	}
 
+		for (IntWritable value : values) {
+			sum += value.get();
+		}
+
+		context.write(key, new IntWritable(sum));
+	}
 }
