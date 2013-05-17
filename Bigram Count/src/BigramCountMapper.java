4396e5db443318820527c3c95fa734aeb609a4e0
Luca Menichetti <meniluca@gmail.com>
2013-05-17 17:51:10 +0200

    Update BigramCountMapper.java

--- Bigram Count/src/BigramCountMapper.java
+++ Bigram Count/src/BigramCountMapper.java
@@ -7,24 +7,21 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class BigramCountMapper extends Mapper<LongWritable, Text, BigramWritable, IntWritable> {
-    private static final IntWritable ONE = new IntWritable(1);
-    private static final BigramWritable BIGRAM = new BigramWritable();
+	private static final IntWritable ONE = new IntWritable(1);
+	private static final BigramWritable BIGRAM = new BigramWritable();
 
 	@Override
-    public void map(LongWritable key, Text value, Context context)
-        throws IOException, InterruptedException {
-      String line = value.toString();
-
-      String prev = null;
-      StringTokenizer itr = new StringTokenizer(line);
-      while (itr.hasMoreTokens()) {
-        String cur = itr.nextToken();
-
-        if (prev != null) {
-          BIGRAM.set(new Text(prev),new Text(cur));
-          context.write(BIGRAM, ONE);
-        }
-        prev = cur;
-      }
-    }
-  }
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+		String line = value.toString();
+		String prev = null;
+      		StringTokenizer itr = new StringTokenizer(line);
+      		while (itr.hasMoreTokens()) {
+        		String cur = itr.nextToken();
+			if (prev != null) {
+				BIGRAM.set(new Text(prev),new Text(cur));
+				context.write(BIGRAM, ONE);
+			}
+			prev = cur;
+		}
+	}	
+}
