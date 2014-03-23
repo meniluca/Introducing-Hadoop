683a24818f5cc5d9a48345b7dda02b7b328edd3f
H4ml3t <meniluca@gmail.com>
2014-03-23 16:27:47 +0100

    removed useless import + indentation

--- Bigram Count/src/BigramCount.java
+++ Bigram Count/src/BigramCount.java
@@ -1,7 +1,4 @@
-import java.io.IOException;
 import java.util.Arrays;
-import java.util.Iterator;
-import java.util.StringTokenizer;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -14,11 +11,8 @@ import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
@@ -27,81 +21,88 @@ import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
 
 public class BigramCount extends Configured implements Tool {
-	
-  private static final Logger LOG = Logger.getLogger(BigramCount.class);
 
-  private BigramCount() {}
+    private static final Logger LOG = Logger.getLogger(BigramCount.class);
 
-  private static final String INPUT = "input";
-  private static final String OUTPUT = "output";
-  private static final String NUM_REDUCERS = "numReducers";
-
-  @SuppressWarnings({ "static-access" })
-  public int run(String[] args) throws Exception {
-    Options options = new Options();
-
-    options.addOption(OptionBuilder.withArgName("path").hasArg().withDescription("input path").create(INPUT));
-    options.addOption(OptionBuilder.withArgName("path").hasArg().withDescription("output path").create(OUTPUT));
-    options.addOption(OptionBuilder.withArgName("num").hasArg().withDescription("number of reducers").create(NUM_REDUCERS));
-
-    CommandLine cmdline;
-    CommandLineParser parser = new GnuParser();
-
-    try {
-      cmdline = parser.parse(options, args);
-    } catch (ParseException exp) {
-      System.err.println("Error parsing command line: " + exp.getMessage());
-      return -1;
+    private BigramCount() {
     }
 
-    if (!cmdline.hasOption(INPUT) || !cmdline.hasOption(OUTPUT)) {
-      System.out.println("args: " + Arrays.toString(args));
-      HelpFormatter formatter = new HelpFormatter();
-      formatter.setWidth(120);
-      formatter.printHelp(this.getClass().getName(), options);
-      ToolRunner.printGenericCommandUsage(System.out);
-      return -1;
+    private static final String INPUT = "input";
+    private static final String OUTPUT = "output";
+    private static final String NUM_REDUCERS = "numReducers";
+
+    @SuppressWarnings({ "static-access" })
+    public int run(String[] args) throws Exception {
+        Options options = new Options();
+
+        options.addOption(OptionBuilder.withArgName("path").hasArg()
+                .withDescription("input path").create(INPUT));
+        options.addOption(OptionBuilder.withArgName("path").hasArg()
+                .withDescription("output path").create(OUTPUT));
+        options.addOption(OptionBuilder.withArgName("num").hasArg()
+                .withDescription("number of reducers").create(NUM_REDUCERS));
+
+        CommandLine cmdline;
+        CommandLineParser parser = new GnuParser();
+
+        try {
+            cmdline = parser.parse(options, args);
+        } catch (ParseException exp) {
+            System.err.println("Error parsing command line: "
+                    + exp.getMessage());
+            return -1;
+        }
+
+        if (!cmdline.hasOption(INPUT) || !cmdline.hasOption(OUTPUT)) {
+            System.out.println("args: " + Arrays.toString(args));
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.setWidth(120);
+            formatter.printHelp(this.getClass().getName(), options);
+            ToolRunner.printGenericCommandUsage(System.out);
+            return -1;
+        }
+
+        String inputPath = cmdline.getOptionValue(INPUT);
+        String outputPath = cmdline.getOptionValue(OUTPUT);
+        int reduceTasks = cmdline.hasOption(NUM_REDUCERS) ? Integer
+                .parseInt(cmdline.getOptionValue(NUM_REDUCERS)) : 1;
+
+        LOG.info("Tool name: " + BigramCount.class.getSimpleName());
+        LOG.info(" - input path: " + inputPath);
+        LOG.info(" - output path: " + outputPath);
+        LOG.info(" - num reducers: " + reduceTasks);
+
+        Job job = new Job(getConf());
+        job.setJobName(BigramCount.class.getSimpleName());
+        job.setJarByClass(BigramCount.class);
+
+        job.setNumReduceTasks(reduceTasks);
+
+        FileInputFormat.setInputPaths(job, new Path(inputPath));
+        FileOutputFormat.setOutputPath(job, new Path(outputPath));
+
+        job.setMapOutputKeyClass(BigramWritable.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        job.setMapperClass(BigramCountMapper.class);
+        job.setReducerClass(BigramCountReducer.class);
+
+        Path outputDir = new Path(outputPath);
+        FileSystem.get(getConf()).delete(outputDir, true);
+
+        long startTime = System.currentTimeMillis();
+        job.waitForCompletion(true);
+        System.out.println("Job Finished in "
+                + (System.currentTimeMillis() - startTime) / 1000.0
+                + " seconds");
+
+        return 0;
     }
 
-    String inputPath = cmdline.getOptionValue(INPUT);
-    String outputPath = cmdline.getOptionValue(OUTPUT);
-    int reduceTasks = cmdline.hasOption(NUM_REDUCERS) ?
-        Integer.parseInt(cmdline.getOptionValue(NUM_REDUCERS)) : 1;
-
-    LOG.info("Tool name: " + BigramCount.class.getSimpleName());
-    LOG.info(" - input path: " + inputPath);
-    LOG.info(" - output path: " + outputPath);
-    LOG.info(" - num reducers: " + reduceTasks);
-
-    Job job = new Job(getConf());
-    job.setJobName(BigramCount.class.getSimpleName());
-    job.setJarByClass(BigramCount.class);
-
-    job.setNumReduceTasks(reduceTasks);
-
-    FileInputFormat.setInputPaths(job, new Path(inputPath));
-    FileOutputFormat.setOutputPath(job, new Path(outputPath));
-
-    job.setMapOutputKeyClass(BigramWritable.class);
-    job.setMapOutputValueClass(IntWritable.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-    job.setOutputFormatClass(TextOutputFormat.class);
-
-    job.setMapperClass(BigramCountMapper.class);
-    job.setReducerClass(BigramCountReducer.class);
-
-    Path outputDir = new Path(outputPath);
-    FileSystem.get(getConf()).delete(outputDir, true);
-
-    long startTime = System.currentTimeMillis();
-    job.waitForCompletion(true);
-    System.out.println("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0 + " seconds");
-
-    return 0;
-  }
-
-  public static void main(String[] args) throws Exception {
-    ToolRunner.run(new BigramCount(), args);
-  }
+    public static void main(String[] args) throws Exception {
+        ToolRunner.run(new BigramCount(), args);
+    }
 }
