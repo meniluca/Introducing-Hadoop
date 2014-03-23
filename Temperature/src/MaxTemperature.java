941bbdc62d79e9c16e03a58bce239d7deee2ada6
H4ml3t <meniluca@gmail.com>
2014-03-23 21:55:49 +0100

    updates

--- Temperature/src/MaxTemperature.java
+++ Temperature/src/MaxTemperature.java
@@ -10,25 +10,25 @@ import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 
 public class MaxTemperature {
-	
-	public static void main(String[] args) throws IOException {
+    
+    public static void main(String[] args) throws IOException {
 
-		JobConf conf = new JobConf(MaxTemperature.class);
-		conf.setJobName("Max temperature");
-		
-		FileInputFormat.addInputPath(conf, new Path("temperature.txt"));
-		FileOutputFormat.setOutputPath(conf, new Path("output"));
-		
-		conf.setMapperClass(MaxTemperatureMapper.class);
-		conf.setReducerClass(MaxTemperatureReducer.class);
-		
-		conf.setMapOutputKeyClass(Text.class);
-		conf.setMapOutputValueClass(IntWritable.class);
-		
-		conf.setOutputKeyClass(Text.class);
-		conf.setOutputValueClass(DoubleWritable.class);
-		
-		JobClient.runJob(conf);
-		
-	}
+        JobConf conf = new JobConf(MaxTemperature.class);
+        conf.setJobName("Max temperature");
+        
+        FileInputFormat.addInputPath(conf, new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+        
+        conf.setMapperClass(MaxTemperatureMapper.class);
+        conf.setReducerClass(MaxTemperatureReducer.class);
+        
+        conf.setMapOutputKeyClass(Text.class);
+        conf.setMapOutputValueClass(IntWritable.class);
+        
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(DoubleWritable.class);
+        
+        JobClient.runJob(conf);
+        
+    }
 }
