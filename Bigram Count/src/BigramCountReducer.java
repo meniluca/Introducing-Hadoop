ccd5126873a897aa421a75428fb07b4cf0545d78
H4ml3t <meniluca@gmail.com>
2014-03-23 16:29:08 +0100

    indent with spaces

--- Bigram Count/src/BigramCountReducer.java
+++ Bigram Count/src/BigramCountReducer.java
@@ -6,18 +6,18 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class BigramCountReducer extends Reducer<BigramWritable, IntWritable, Text, IntWritable> {
-	
-	private final static IntWritable SUM = new IntWritable();
-	
-	@Override
-	public void reduce(BigramWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-		int sum = 0;
-		Iterator<IntWritable> iter = values.iterator();
-		while (iter.hasNext()) {
-			sum += iter.next().get();
-		}
-		SUM.set(sum);
-		context.write(new Text(key.toString()), SUM);
-	}
-	
+    
+    private final static IntWritable SUM = new IntWritable();
+    
+    @Override
+    public void reduce(BigramWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int sum = 0;
+        Iterator<IntWritable> iter = values.iterator();
+        while (iter.hasNext()) {
+            sum += iter.next().get();
+        }
+        SUM.set(sum);
+        context.write(new Text(key.toString()), SUM);
+    }
+    
 }
